bc6c945c7dfca19a3cc69897d235b9864f8a94ba
Den Beckett <den_local@debian>
2023-03-12 00:21:11 +0300

    Seminar03fix

--- sem_java/sem_java/src/main/java/org/example/Seminar3/Ex11/Ex11.java
+++ sem_java/sem_java/src/main/java/org/example/Seminar3/Ex11/Ex11.java
@@ -1,14 +1,13 @@
 package org.example.Seminar3.Ex11;
 
-import java.util.Scanner;
-import org.example.Seminar3.Ex11.model.Student;
+import org.example.Seminar3.Ex11.Model.Student;
 import java.util.List;
 import java.util.ArrayList;
 
 public class Ex11 {
     public static void main(String[] args) {
         Student student1 = new Student("Иванова", 2, 237, List.of(6, 4, 5));
-        Student student2 = new Student("Машков", 3, 234, List.of(3, 4, 5));
+        Student student2 = new Student("Машков", 3, 234, List.of(3, 4,5));
         Student student3 = new Student("Котлова", 2, 234, List.of(3, 4, 5));
         Student student4 = new Student("Михин", 5, 234, List.of(3, 4, 5));
     
